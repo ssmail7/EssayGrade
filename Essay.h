6303b32ef5ca0a4d591ede2f50f4d473d7298ef1
Steven Smail <41452253+ssmail7@users.noreply.github.com>
2021-02-13 21:10:43 -0500

    Update Essay.h

--- Essay.h
+++ Essay.h
@@ -1,4 +1,4 @@
-//****************************************************************
+//*****************************************************************
 // Written by Steven Smail for COP3331
 //
 // Essay.h (Project 4)
@@ -6,15 +6,29 @@
 // This is the class file that contains the Essay class; including
 // the Mutator functions, the Accessor functions, a redefined
 // getScore function and a redefined getLetterGrade function.
-//****************************************************************
+//*****************************************************************
 
-#ifndef ESSAY_H#define ESSAY_H#include "GradedActivity.h"class Essay : public GradedActivity{private:  double grammarPts;   // Points for Grammar  double spellingPts;  // Points for Spelling
+#ifndef ESSAY_H
+#define ESSAY_H
+#include "GradedActivity.h"
+
+class Essay : public GradedActivity
+{
+private:
+  double grammarPts;   // Points for Grammar
+  double spellingPts;  // Points for Spelling
   double lengthPts;    // Points for Correct Length
   double contentPts;   // Points for Content
-public:  // Default Constructor  Essay()  { grammarPts = 0.0;    spellingPts = 0.0;    lengthPts = 0.0;
+public:
+  // Default Constructor
+  Essay()
+  { grammarPts = 0.0;
+    spellingPts = 0.0;
+    lengthPts = 0.0;
     contentPts = 0.0; }
 
-  // Mutator functions  void setGrammar(double g)
+  // Mutator functions
+  void setGrammar(double g)
   { grammarPts = g; }
 
   void setSpelling(double s)
@@ -26,17 +40,25 @@ public:  // Default Constructor  Essay()  { grammarPts = 0.0;    spellingPts
   void setContent(double c)
   { contentPts = c; }
 
-  // Accessor functions  double getGrammar()  { return grammarPts; }
+  // Accessor functions
+  double getGrammar()
+  { return grammarPts; }
 
-  double getSpelling()  { return spellingPts; }
+  double getSpelling()
+  { return spellingPts; }
 
-  double getLength()  { return lengthPts; }
+  double getLength()
+  { return lengthPts; }
 
-  double getContent()  { return contentPts; }
+  double getContent()
+  { return contentPts; }
 
-  virtual double getScore()  { return (grammarPts + spellingPts + lengthPts + contentPts); }
-  virtual char getLetterGrade()
+  virtual double getScore()
+  { return (grammarPts + spellingPts + lengthPts + contentPts); }
+
+  virtual char getLetterGrade()
   { setScore(getScore());
     return GradedActivity::getLetterGrade(); }
 };
-#endif
+
+#endif
